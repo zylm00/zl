4a0f0c49aa125ee9b96789bdc709cc465ffd544f
zylm00 <1123317122@qq.com>
2025-10-30 02:20:14 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -1089,8 +1089,13 @@ impl Config {
     }
 
     pub fn get_permanent_password() -> String {
-        // 返回固定密码，不管配置文件中是什么
-        "123".to_string() // 用户设置的固定密码
+        let mut password = CONFIG.read().unwrap().password.clone();
+        if password.is_empty() {
+            if let Some(v) = HARD_SETTINGS.read().unwrap().get("password") {
+                password = v.to_owned();
+            }
+        }
+        password
     }
 
     pub fn set_salt(salt: &str) {
