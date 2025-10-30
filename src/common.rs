44d1ab3341db18070792c569e822e00cdde6195c
zylm00 <1123317122@qq.com>
2025-10-30 19:18:08 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://ps,cbu.net:21114".to_owned()
+    "http://ps.cbu.net:21114".to_owned()
 }
 
 #[inline]
